a5d1a853b36c92723332a2ed74e3f9c41f1e9f7d
Matouš Kučera <mk@kcra.me>
2024-05-15 18:50:39 +0200

    feat: generate nekos v2 api client

--- generate.go
+++ generate.go
@@ -6,6 +6,7 @@ package nero
 // - expose the http.Request for strict response visit
 
 //go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen@v2.1.0 --config ./server/api/nekos/v2/models.cfg.yaml -o ./server/api/nekos/v2/models.gen.go ./server/api/schema/nekos/v2.yaml
+//go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen@v2.1.0 --config ./server/api/nekos/v2/client.cfg.yaml -o ./server/api/nekos/v2/client.gen.go ./server/api/schema/nekos/v2.yaml
 //go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen@v2.1.0 --config ./server/api/nekos/v2/server.cfg.yaml --templates ./server/api/templates -o ./server/api/nekos/v2/server.gen.go ./server/api/schema/nekos/v2.yaml
 
 //go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen@v2.1.0 --config ./server/api/v1/models.cfg.yaml -o ./server/api/v1/models.gen.go ./server/api/schema/v1.yaml
